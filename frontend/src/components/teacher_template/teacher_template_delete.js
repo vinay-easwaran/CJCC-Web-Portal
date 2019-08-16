5d180429d5d09e915940dc21ca2175c3931804af
vinay-easwaran <vinayeaswaran@yahoo.co.in>
2019-08-15 17:35:19 -0700

    delete working for teacher

--- frontend/src/components/teacher_template/teacher_template_delete.js
+++ frontend/src/components/teacher_template/teacher_template_delete.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './teacher_template.css';
+import axios from 'axios';
 import Navigation from '../Navigation'
 
 class Teacher_template_form_delete extends React.Component {
@@ -17,12 +18,12 @@ onSubmit = (e) => {
   e.preventDefault();
   console.log(this.state);
   axios.delete('/api/teachers/' + this.state.teacher_id, {
-  	data: {teacher_id: this.state.teacher_id, }
-  }
+  	data: { teacher_id: this.state.teacher_id, }
+  })
   .then(function (response) {
   	console.log(response);
   })
-  .catch(function (error)) {
+  .catch(function (error) {
   	console.log(error);
   });
   
@@ -43,7 +44,8 @@ onSubmit = (e) => {
           value = {this.state.teacher_id}
           onChange={e => this.change(e)}
         />
-        <br />
+        <br/>
+        <br/>
         <button onClick={e => this.onSubmit(e)}>Submit</button>
         <br />
       </form>
